439d2af8f18524b614c2adb7507d5757b97a63fa
Hateem729 <hateemzainab5@gmail.com>
2026-07-24 03:14:35 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-document.getElementById("startBtn").addEventListener("click", function () {
 
-    alert("Welcome ENGENE! Let's find your bias.");
+const page1 = document.getElementById("page1");
+const page2 = document.getElementById("page2");
+
+document.getElementById("startBtn").onclick = () => {
+
+page1.classList.add("hidden");
+
+page2.classList.remove("hidden");
+
+};
+
+const members = document.querySelectorAll(".member");
+
+members.forEach(member=>{
+
+member.onclick=()=>{
+
+localStorage.setItem("bias",member.dataset.member);
+
+alert("You selected "+member.innerText);
+
+};
 
 });
